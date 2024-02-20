bfb2dbd053d4ffd5c3cb5a6b199fb0ff9cbbe52a
Emilio Guisolfo <emilioguisolfo@gmail.com>
2024-02-20 12:51:44 -0300

    feat: bumped signpost version to display openstreet map (#25)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,11 +1,8 @@
 import { AlgoliaSearchIndex } from '@ircsignpost/signpost-base/dist/src/search-common';
-import { LatLngExpression } from 'leaflet';
 
 export const SITE_TITLE = 'ImportaMi';
 
 export const COUNTRY_ID = 31;
-
-export const MAP_DEFAULT_COORDS: LatLngExpression = [37.0902, -95.7129];
 
 // Cache statically generated pages for 1 hour. The timeout was chosen
 // arbitrarily. Our website has static, non-urgent resources, so we probably do
